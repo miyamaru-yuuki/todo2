d8c2b742acd2ee3fa6040f1983bea596cd74cfc4
miyamaruyuuki <ajptmjg@gmail.com>
2020-04-29 16:01:04 +0900

    1601

--- updateKakunin.php
+++ updateKakunin.php
@@ -1,6 +1,5 @@
 <?php
 require_once ('function.php');
-require_once ('todotable_class.php');
 
 if(!isset($_POST['tid'],$_POST['tname'],$_POST['status'],$_POST['priority'])) {
     header("Location: http://mmr.e5.valueserver.jp/todo2/index.php?error=1");
